d30869e9719f424269d8692a079894188b98e3f3
developpeur.olive as Oliver <olivier.codes@gmail.com>
2024-05-21 19:24:53 +0200

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by olivier geiger on 21/05/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
