ec1415d74ffa1a4036ca03b1388c89513975ba3d
developpeur.olive as Oliver <olivier.codes@gmail.com>
2024-05-21 18:15:16 +0200

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by olivier geiger on 21/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
