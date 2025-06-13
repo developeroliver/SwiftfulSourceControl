4210e87d06f28223c647c21741bdf4fdb6750076
codewitholivier as Oliver <developpeur.olive@gmail.com>
2025-06-13 15:46:19 +0200

    Day 19

---
+++ SwiftfulSourceControl/SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by olivier geiger on 13/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
