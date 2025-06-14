4ce139f566fb3bf39d8f4dbca12b7a72ecd4b48b
codewitholivier as Oliver <developpeur.olive@gmail.com>
2025-06-14 16:38:12 +0200

    [Clean] Add commit message documentation

--- SwiftfulSourceControl/SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/SwiftfulSourceControl/ContentView.swift
@@ -5,6 +5,26 @@
 //  Created by olivier geiger on 13/06/2025.
 //
 
+/*
+ COMMIT MESSAGES
+ 
+ NEW FEATURE:
+ [Feature] Description of the feature
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of patch
+ 
+ BUG NOT IN PRODUCTION
+ [Bug] Description of the bug
+ 
+ MUNDANE TASKS:
+ [Clean] Description of changes
+ 
+ RELEASE:
+ [Release] Description of release
+ 
+ */
+
 import SwiftUI
 
 struct ContentView: View {
