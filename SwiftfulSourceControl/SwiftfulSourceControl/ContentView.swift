4e807f9463bf8fab06eef900c600eeae515c89aa
codewitholivier as Oliver <developpeur.olive@gmail.com>
2025-06-14 16:19:14 +0200

    My first commit

--- SwiftfulSourceControl/SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/SwiftfulSourceControl/ContentView.swift
@@ -11,9 +11,9 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
-                .imageScale(.large)
+                .font(.largeTitle)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Swiftful Thinking")
         }
         .padding()
     }
